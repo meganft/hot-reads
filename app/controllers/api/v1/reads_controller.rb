cf33bbf1685d7ac9cfe97504b6c0003d49ee6926
Megan Talbot <meganftalbot@gmail.com>
2017-02-13 18:22:03 -0700

    Change params and render code

--- app/controllers/api/v1/reads_controller.rb
+++ app/controllers/api/v1/reads_controller.rb
@@ -1,10 +1,12 @@
 class Api::V1::ReadsController < ApplicationController
+   skip_before_filter :verify_authenticity_token
 
   def create
     @read = Read.create(title: params[:link][:title], url: params[:link][:url])
+    render json: @read, status: 200
   end
 
   private
-    # def read_params
-    # end
+    def read_params
+    end
 end
