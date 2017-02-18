893a1978e95833d68be5b8398a16f8c5fc29df73
Megan Talbot <meganftalbot@gmail.com>
2017-02-18 09:37:13 -0700

    Update method by count

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,6 +1,6 @@
 class Read < ActiveRecord::Base
 
   def self.top_ten
-    where(created_at: (Time.now - 24.hours)..Time.now)
+    where('created_at > ?', 24.hours.ago).group('url').limit(10).count
   end
 end
