7e09f51971066bc3b08c22699db0a3591d84b0e1
Megan Talbot <meganftalbot@gmail.com>
2017-02-18 08:58:54 -0700

    Fix typo

--- app/controllers/api/v1/reads_controller.rb
+++ app/controllers/api/v1/reads_controller.rb
@@ -2,7 +2,7 @@ class Api::V1::ReadsController < ApplicationController
    skip_before_filter :verify_authenticity_token
 
   def create
-    puts params
+    puts params.inspect
     @read = Read.create(title: params[:link][:title], url: params[:link][:url])
     render json: @read, status: 200
   end
