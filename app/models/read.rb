26a5103b8919aa052d83a09d249e4a2dcc5b5d5e
Megan Talbot <meganftalbot@gmail.com>
2017-02-18 09:39:02 -0700

    Change model back, not working properly

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,6 +1,7 @@
 class Read < ActiveRecord::Base
 
   def self.top_ten
-    where('created_at > ?', 24.hours.ago).group('url').limit(10).count
+    where('created_at > ?', 24.hours.ago)
+    # .group('url').limit(10).count
   end
 end
