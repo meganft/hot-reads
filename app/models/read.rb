5ae5ba6fbe991c2ea7d9b5f580037647953e7c09
Megan Talbot <meganftalbot@gmail.com>
2017-02-18 09:32:57 -0700

    Change render method

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,6 +1,6 @@
 class Read < ActiveRecord::Base
 
   def self.top_ten
-    where(created_at: (Time.now - 24.hours)..Time.now).count
+    where(created_at: (Time.now - 24.hours)..Time.now)
   end
 end
