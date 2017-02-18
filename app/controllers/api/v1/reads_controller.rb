4f7ff61bc9c8e25e9534573305be1e692ad88b91
Megan Talbot <meganftalbot@gmail.com>
2017-02-18 08:56:47 -0700

    puts params

--- app/controllers/api/v1/reads_controller.rb
+++ app/controllers/api/v1/reads_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ReadsController < ApplicationController
    skip_before_filter :verify_authenticity_token
 
   def create
+    puts parasms
     @read = Read.create(title: params[:link][:title], url: params[:link][:url])
     render json: @read, status: 200
   end
