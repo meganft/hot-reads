798df8b2744bed3af8c792e69ec41d4982ba26ad
Megan Talbot <meganftalbot@gmail.com>
2017-02-25 13:20:44 -0700

    Add rb readline gem

--- app/models/read.rb
+++ app/models/read.rb
@@ -1,7 +1,6 @@
 class Read < ActiveRecord::Base
 
   def self.top_ten
-    where(created_at: (Time.now - 24.hours)..Time.now).group(:url).count.sort_by { |key, val| val }.reverse[0..9]
-    # .group('url').limit(10).count
+    where('created_at > ?', 24.hours.ago).group('url').limit(10).count
   end
 end
